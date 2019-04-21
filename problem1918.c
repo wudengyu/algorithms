1c5ac579881267cfd01d37585185f219467875f1
wudengyu <wudengyu@tom.com>
2019-04-21 17:58:48 +0800

    通过Bella 姐姐发辣条

--- problem1918.c
+++ problem1918.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int t,n,a[100],b[100];
-    int last,sum,plus=0;
+    int last,sum;
     scanf("%d",&t);
     while(t--){
         scanf("%d",&n);
@@ -9,17 +9,16 @@ int main(){
             scanf("%d",&a[i]);
         for(int i=0;i<n;i++)
             scanf("%d",&b[i]);
-        last=a[0];
-        sum=a[0];
-        for(int i=1;i<n;i++){
+        last=0;
+        sum=0;
+        for(int i=0;i<n;i++){
             if(last+b[i]>a[i]){
-                last+=last+b[i];
-                sum=sum+last;
+                last+=b[i];
             }else{
-                plus=a[i]-b[i]-last;
+                sum+=i*(a[i]-b[i]-last);
                 last=a[i];
-                sum=sum+last+i*plus;
             }
+            sum+=last;
         }
         printf("%d\n",sum);
     }
