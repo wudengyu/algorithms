9fecb7e6f170e14e43292d3331127002ace0f5df
吴登宇 <wudengyu@wudengyudeiMac.local>
2019-10-07 19:50:38 +0800

    增加一个LCS

---
+++ LCS.cpp
@@ -0,0 +1,65 @@
+/*最长公共子序列（LCS Longest-Common-Subsequence Problem）
+ *动态规划
+*/
+#include <iostream>
+#include <cstring>
+#define MOD 100000000
+using namespace std;
+int main(){
+    string x,y;
+    while(!getline(cin,x).eof()&&!getline(cin, y).eof())
+    {
+        int xl=x.length()-1;
+        int yl=y.length()-1;
+        x=' '+x;
+        y=' '+y;
+        int **dp=new int*[xl+1];
+        int **sum=new int*[xl+1];
+        for(int i=0;i<=xl;i++){
+            dp[i]=new int[yl+1];
+            sum[i]=new int[yl+1];
+        }
+        for(int i=0;i<=xl;i++){
+            dp[i][0]=0;
+            sum[i][0]=1;
+        }
+        for(int j=0;j<=yl;j++){
+            dp[0][j]=0;
+            sum[0][j]=1;
+        }
+        for(int i=1;i<=xl;i++){
+            for(int j=1;j<=yl;j++){
+                if (x[i]==y[j]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    sum[i][j]=sum[i-1][j-1];
+                    if(dp[i][j]==dp[i][j-1])
+                        sum[i][j]=(sum[i][j]+sum[i][j-1])%MOD;
+                    if(dp[i][j]==dp[i-1][j])
+                        sum[i][j]=(sum[i][j]+sum[i-1][j])%MOD;
+                }else{
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+                    if(dp[i][j]==dp[i][j-1])
+                        sum[i][j]=(sum[i][j]+sum[i][j-1])%MOD;
+                    if(dp[i][j]==dp[i-1][j])
+                        sum[i][j]=(sum[i][j]+sum[i-1][j])%MOD;
+                    if(dp[i][j]==dp[i-1][j-1]){
+                        sum[i][j]=(sum[i][j]-sum[i-1][j-1]+MOD)%MOD;
+                        cout<<sum[i][j]<<' '<<endl;
+                    }
+                }
+            }
+        }
+        cout<<dp[xl][yl]<<endl;
+        if(dp[xl][yl]==0)
+            cout<<0<<endl;
+        else
+            cout<<sum[xl][yl]<<endl;
+        for(int i=0;i<xl;i++)
+        {
+            delete[] * (dp + i);
+            delete[] * (sum + i);
+        }
+        delete[] dp;
+        delete[] sum;
+    }
+}
--- problem96.cpp
+++ problem96.cpp
@@ -7,56 +7,63 @@
 using namespace std;
 int main(){
     string x,y;
+    int *dp[2],*sum[2];
     while(!getline(cin,x).eof()&&!getline(cin, y).eof())
     {
         int xl=x.length()-1;
         int yl=y.length()-1;
         x=' '+x;
         y=' '+y;
-        int **dp=new int*[xl+1];
-        int **sum=new int*[xl+1];
         for(int i=0;i<2;i++){
             dp[i]=new int[yl+1];
             sum[i]=new int[yl+1];
         }
-        for(int i=0;i<=xl;i++){
+        for(int i=0;i<2;i++){
             dp[i][0]=0;
-            sum[i][0]=1;
+            sum[i][0]=0;
         }
         for(int j=0;j<=yl;j++){
             dp[0][j]=0;
-            sum[0][j]=1;
+            sum[0][j]=0;
         }
         for(int i=1;i<=xl;i++){
+            int curr=i&1;
+            int prev=curr^1;
             for(int j=1;j<=yl;j++){
-                if (x[i]==y[j])
-                    dp[i][j]=dp[i-1][j-1]+1;
-                    sum[i][j]=sum[i-1][j-1];
-                    if(dp[i][j]==dp[i][j-1])
-                        sum[i][j]=(sum[i][j]+sum[i][j-1])%MOD;
-                    if(dp[i][j]==dp[i-1][j])
-                        sum[i][j]=(sum[i][j]+sum[i-1][j])%MOD;
-                else
-                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-                    if(dp[i][j]==dp[i][j-1])
-                        sum[i][j]=(sum[i][j]+sum[i][j-1])%MOD;
-                    if(dp[i][j]==dp[i-1][j])
-                        sum[i][j]=(sum[i][j]+sum[i-1][j])%MOD;
-                    if(dp[i][j]==dp[i-1][j-1])
-                        sum[i][j]=(sum[i][j]-sum[i-1][j]+MOD)%MOD;
+                if (x[i]==y[j]){
+                    dp[curr][j]=dp[prev][j-1]+1;
+                    if(sum[prev][j-1]==0)
+                        sum[curr][j]=1;
+                    else
+                        sum[curr][j]=sum[prev][j-1];
+                    if(dp[curr][j]==dp[curr][j-1])
+                        sum[curr][j]=(sum[curr][j]+sum[curr][j-1])%MOD;
+                    if(dp[curr][j]==dp[prev][j])
+                        sum[curr][j]=(sum[curr][j]+sum[prev][j])%MOD;
+                }else{
+                    dp[curr][j]=max(dp[prev][j], dp[curr][j-1]);
+                    sum[curr][j]=0;
+                    if(dp[curr][j]==dp[curr][j-1])
+                        sum[curr][j]=(sum[curr][j]+sum[curr][j-1])%MOD;
+                    if(dp[curr][j]==dp[prev][j])
+                        sum[curr][j]=(sum[curr][j]+sum[prev][j])%MOD;
+                    if(dp[curr][j]==dp[prev][j-1])
+                        sum[curr][j]=(sum[curr][j]-sum[prev][j-1]+MOD)%MOD;
+                }
             }
+            for(int tmp=0;tmp<=yl;tmp++)
+                cout<<dp[curr][tmp]<<':'<<sum[curr][tmp]<<' ';
+            cout<<endl;
         }
-        cout<<dp[xl][yl]<<endl;
-        if(dp[xl][yl]==0)
+        cout<<dp[xl&1][yl]<<endl;
+        if(dp[xl&1][yl]==0)
             cout<<0<<endl;
         else
-            cout<<sum[xl][yl]<<endl;
-        for(int i=0;i<xl;i++)
+            cout<<sum[xl&1][yl]<<endl;
+        for(int i=0;i<2;i++)
         {
             delete[] * (dp + i);
             delete[] * (sum + i);
         }
-        delete[] dp;
-        delete[] sum;
     }
 }
