2fceff1453c0651a1a241e71f9c33937fde85e66
吴登宇 <wudengyu@wudengyudeMac-mini.local>
2019-10-18 17:47:02 +0800

    jbb

---
+++ problem143.cpp
@@ -0,0 +1,16 @@
+/*机智零崎不会没梗Ⅰ
+ *看似背包，实际算不上，因为不需要DP
+*/
+#include <iostream>
+using namespace std;
+int w[10000],v[10000];//w重量，v价值
+int main(){
+    int g,k,n;//g背包承重，k物品数量，n价值期望
+    while(!(cin>>g>>k>>n).eof()){
+        for(int i=0;i<k;i++)
+            cin>>w[i]>>v[i];
+    }
+
+
+
+}
