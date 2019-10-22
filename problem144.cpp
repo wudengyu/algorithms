1f9baaec9d633f36e6c0e067aa25921f6e26098e
吴登宇 <wudengyu@wudengyudeMac-mini.local>
2019-10-22 15:37:29 +0800

    矩形填充问题，丑

--- problem144.cpp
+++ problem144.cpp
@@ -4,80 +4,47 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int baskets[8][6]={{0,0,0,0,0,1},{11,0,0,0,1,0},{0,5,0,1,0},{0,0,4,0},
-    {6,3,2,0},{7,5,1,0},{0,9,0},{36,0}};
 int main(){
     int h[6];
-    int use[8];
-    int sum=0;
+    int used;
     while(true){
         for(int i=0;i<6;i++){
             if((cin>>h[i]).eof())
                 return 0;
         }
-        memset(use,0,sizeof(use));
-        use[0]=h[5];
-        use[1]=h[4];
-        use[2]=h[3];
-        use[3]=h[2]/4;
-        for(int i=0;i<4;i++){
-            for(int j=0;j<6;j++)
-                h[j]-=baskets[i][j]*use[i];
-        }
-        if(h[1]<0){
-            h[0]-=-h[1]*4;
-            h[1]=0;
-        }
-        if(h[2]==3){
-            use[3]++;
-            h[2]=0;
-            if(h[1]>0){
-                h[1]-=1;
-                h[0]-=3;
-            }else
-                h[0]-=9;
-        }else if(h[2]==2){
-            use[4]++;
-            h[2]=0;
-            if(h[1]>=3){
-                h[1]-=3;
-                h[0]-=6;
-            }else{
-                h[0]=h[0]-6-(3-h[1])*4;
-                h[1]=0;
+        used=0;
+        used+=h[5];//处理6×6
+        for(int i=4;i>=3;i--){
+            used+=h[i];
+            h[4-i]-=h[i]*(6+(i+1))/(6-(i+1));
+        }//处理5×5、4×4
+        for(int i=2;i>=0;i--){
+            if(h[i]>0){
+                used+=h[i]/(36/((i+1)*(i+1)));
+                h[i]%=36/((i+1)*(i+1));
+                if(h[i]>0){
+                    used++;
+                    h[i]-=36/((i+1)*(i+1));
+                }
             }
-        }else if(h[2]==1){
-            use[5]++;
-            h[2]=0;
-            if(h[1]>=5){
-                h[1]-=5;
-                h[0]-=7;
-            }else{
-                h[0]=h[0]-7-(5-h[1])*4;
-                h[1]=0;
+            if(i>0&&h[i]<0){
+                if(i==2&&h[i]==-3){//剩于3×3等于3个时的特殊处理
+                    h[i]+=3;
+                    h[1]-=5;
+                    h[0]-=7;
+                }
+                if(i==2&&h[i]==-2){//剩于3×3等于2个时的特殊处理
+                    h[i]+=2;
+                    h[1]-=3;
+                    h[0]-=6;
+                }
+                while(h[i]<0){
+                    h[i]++;
+                    h[i-1]--;
+                    h[0]-=2*i+1;
+                }
             }
         }
-        if(h[1]>0){
-            use[6]=h[1]/9;
-            h[1]%=9;
-            if(h[1]>0){
-                use[6]++;
-                h[0]-=36-4*h[1];
-                h[1]=0;
-            }
-        }
-        if(h[0]>0){
-            use[7]=h[0]/36;
-            h[0]%=36;
-            if(h[0]>0)
-                use[7]++;
-        }
-        sum=0;
-        for(int i=0;i<8;i++){
-            sum+=use[i];
-            cout<<use[i]<<' ';
-        }
-        cout<<endl;
-        cout<<sum<<endl;
-        }
+        cout<<used<<endl;
+    }
 }
