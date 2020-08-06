53e003bd95bf10048c1b94435a9796740ed4bf2c
wudengyu <wudengyu@DESKTOP-F8T32UD.localdomain>
2020-08-06 18:00:13 +0800

    asdas

--- sources/lougu3803.cpp
+++ sources/lougu3803.cpp
@@ -2,9 +2,19 @@
 #include <complex>
 #include <cmath>
 using namespace std;
+const int mod=998244353;
 
-complex<double> p[2097152],g[2097152];//两多项式最高可达2*10^6次，系数则有2*10^6+1项，但是傅里叶变换需要把系数扩展到2的整数次方，所有这里预留的2^21，大多数是直接扩展4倍。
-void fft(complex<double> a[],int len,int inv){
+int p[2097152],g[2097152];//两多项式最高可达2*10^6次，系数则有2*10^6+1项，但是傅里叶变换需要把系数扩展到2的整数次方，所有这里预留的2^21，大多数是直接扩展4倍。
+unsigned long long pow(unsigned long long x,int y,int mod){
+    unsigned long long p=1;
+    while(y){
+        if(y&1)p=x*p%mod;
+        x=x*x%mod;
+        y>>=1;
+    }
+    return p;
+}
+void ntt(int a[],int len,int inv){
     for(int i=0,j=0;i<len;i++){//采用雷德算法进行位逆序变换，很多写法i直接从1开始，因为0显然是不需要变换位置，所以不影响结果。
         int k=len;//len是2的整数次幂，决定了二进制的位数；
         if(i<j)swap(a[i],a[j]);
@@ -13,52 +23,46 @@ void fft(complex<double> a[],int len,int inv){
         j|=k;//前一步只是处理了需要进位的，不需要进位之后，置1（或运算）
     }
     for(int s=2;s<=len;s<<=1){//s是准备合并序列的长度
-        complex<double> wm(cos(inv*2*M_PI/s),sin(inv*2*M_PI/s));
+        unsigned long long wm=pow(3,(mod-1)/s);
+        if(inv==-1)wm=pow(wm,mod-2);
         for(int k=0;k<len;k+=s){//步长是序列的长度，循环一次处理一个序列
-            complex<double> w(1,0);
+            int w=1;
             for(int j=0;j<s/2;j++){//前一半和后一半合并，所以循环终止条件是到k+s/2
-                complex<double> t=w*a[k+j+s/2];
-                complex<double> u=a[k+j];
-                a[k+j]=u+t;
-                a[k+j+s/2]=u-t;
-                w=w*wm;
+                unsigned long long t=(unsigned long long)w*a[k+j+s/2]%mod;
+                unsigned long long u=a[k+j];
+                a[k+j]=(u+t)%mod;
+                a[k+j+s/2]=(u-t+mod)%mod;
+                w=(unsigned long long)w*wm%mod;
             }
         }
     }
-    if(inv==-1)
+    if(inv==-1){
+        unsigned long long t=pow(len,mod-2);
         for(int i=0;i<len;i++)
-            a[i].real(a[i].real()/len);
+            a[i]=a[i]*t%mod;
+    }
 }
 int main(){
     int n,m,limit=1,temp;
     cin>>n>>m;
     while(limit<n+m+1)limit<<=1;
-    for(int i=0;i<=n;i++){
-        cin>>temp;
-        p[i].real(temp);
-        p[i].imag(0);
-    }
-    for(int i=0;i<=m;i++){
-        cin>>temp;
-        g[i].real(temp);
-        g[i].imag(0);
-    }
-    for(int i=n+1;i<limit;i++){
-        p[i].real(0);
-        p[i].imag(0);
-    }
-    for(int i=m+1;i<limit;i++){
-        g[i].real(0);
-        g[i].imag(0);
-    }
-    fft(p,limit,1);
-    fft(g,limit,1);
+    for(int i=0;i<=n;i++)
+        cin>>p[i];
+    for(int i=0;i<=m;i++)
+        cin>>g[i];
+    for(int i=n+1;i<limit;i++)
+        p[i]=0;
+    for(int i=m+1;i<limit;i++)
+        g[i]=0;
+    ntt(p,limit,1);
+    ntt(g,limit,1);
     for(int i=0;i<limit;i++){
-        p[i]=p[i]*g[i];
+        p[i]=(unsigned long long)p[i]*g[i]%mod;
+        cout<<"p["<<i<<"]="<<p[i]<<",g["<<i<<"]="<<g[i]<<" ";
     }
-    fft(p,limit,-1);
+    ntt(p,limit,-1);
     for(int i=0;i<n+m+1;i++){
-        cout<<(int)(p[i].real()+0.5)<<" ";
+        cout<<p[i]<<" ";
     }
     cout<<endl;
 }
--- sources/problem1415.cpp
+++ sources/problem1415.cpp
@@ -3,11 +3,20 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
+const int MOD=998244353;
 const int N=2<<20; //一般开出4倍空间，实际最大可能用到2倍再扩展到2的整数次方，所以这里取2^21
 char x[N],y[N];
-complex<double> f[N], g[N];
-int sum[N];
-void fft(complex<double> a[],int len,int inv){
+int f[N],g[N],sum[N];
+int pow(unsigned long long x,int y) {//快速幂算法
+    unsigned long long p=1;
+    while(y){
+        if(y&1)p=x*p%MOD;
+        x=x*x%MOD;
+        y>>=1;
+    }
+    return p;
+}
+void ntt(int a[],int len,int inv){
     for(int i=0,j=0;i<len;i++){//采用雷德算法进行位逆序变换，很多写法i直接从1开始，因为0显然是不需要变换位置，所以不影响结果。
         int k=len;//len是2的整数次幂，决定了二进制的位数；
         if(i<j)swap(a[i],a[j]);
@@ -16,52 +25,58 @@ void fft(complex<double> a[],int len,int inv){
         j|=k;//前一步只是处理了需要进位的，不需要进位之后，置1（或运算）
     }
     for(int s=2;s<=len;s<<=1){//s是准备合并序列的长度
-        complex<double> wm(cos(inv*2*M_PI/s),sin(inv*2*M_PI/s));
-        for(int k=0;k<len;k+=s){//步长是序列的长度，循环一次处理一个序列
-            complex<double> w(1,0);
+        int gm=pow(3,(MOD-1)/s);//3是原根
+        if(inv==-1)gm=pow(gm,MOD-2);
+         for(int k=0;k<len;k+=s){//步长是序列的长度，循环一次处理一个序列
+            int g=1;
             for(int j=0;j<s/2;j++){//前一半和后一半合并，所以循环终止条件是到k+s/2
-                complex<double> t=w*a[k+j+s/2];
-                complex<double> u=a[k+j];
-                a[k+j]=u+t;
-                a[k+j+s/2]=u-t;
-                w=w*wm;
+                unsigned long long t=(long long)g*a[k+j+s/2]%MOD;
+                unsigned long long u=a[k+j];
+                a[k+j]=(u+t)%MOD;
+                a[k+j+s/2]=(u-t+MOD)%MOD;
+                g=(long long)g*gm%MOD;
             }
         }
     }
-    if(inv==-1)
-        for(int i=0;i<len;i++)
-            a[i].real(a[i].real()/len);
+    if(inv==-1){
+		int t=pow(len,MOD-2);
+		for (int i=0;i<len;i++)
+			a[i]=a[i]*t%MOD;
+	}
 }
 void solve(char c,int n,int m,int len){
     for(int i=0;i<n;i++){
         if(x[i]==c)
-            f[i].real(1);
+            f[i]=1;
         else
-            f[i].real(0);
-        f[i].imag(0);
+            f[i]=0;
     }
     for(int i=n;i<len;i++){
-        f[i].real(0);
-        f[i].imag(0);
+        f[i]=0;
     }
     for(int i=0;i<m;i++){
         if(y[i]==c)
-            g[i].real(1);
+            g[i]=1;
         else
-            g[i].real(0);
-        g[i].imag(0);
+            g[i]=0;
     }
     for(int i=m;i<len;i++){
-        g[i].real(0);
-        g[i].imag(0);
+        g[i]=0;
     }
-    fft(f,len,1);
-    fft(g,len,1);
+    ntt(f,len,1);
+    for(int i=0;i<len;i++){
+        cout<<f[i]<<" "; 
+    }
+    cout<<endl;
+    ntt(g,len,1);
     for(int i=0;i<len;i++)
-        f[i]=f[i]*g[i];
-    fft(f,len,-1);
-    for(int i=0;i<len;i++)
-        sum[i]+=(int)(f[i].real()+0.5);
+        f[i]=(unsigned long long)f[i]*g[i]%MOD;
+    ntt(f,len,-1);
+    for(int i=0;i<len;i++){
+        sum[i]+=f[i];
+        cout<<f[i]<<" ";
+    }
+    cout<<endl;
 }
 int main(){
     int n,m,len=1,ans=0;
@@ -85,7 +100,7 @@ int main(){
     solve('S',n,m,len);
     solve('P',n,m,len);
     for(int i=m-1;i<n+m-1;i++)
-        ans=max(ans,sum[i]);
+        ans=max(ans,(int)sum[i]);
     printf("%d\n",ans);
     return 0;
 }
