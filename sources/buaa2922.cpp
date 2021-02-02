39d52f5b53bd8a6b45f47e1d9e6ca9c582664835
wudengyu <wudengyu@tom.com>
2021-02-03 00:39:07 +0800

    完成Dijkstra算法

--- sources/buaa2922.cpp
+++ sources/buaa2922.cpp
@@ -2,55 +2,56 @@
 #include<vector>
 #include<queue>
 #include<map>
+#include<set>
 #include<forward_list>
+#include<limits>
 using namespace std;
-int vomit[501];
+const int N=501;
 struct egde{
     int from;
     int to;
     int cost;
-    bool operator<(const egde &a) const{
-        return cost>a.cost;
-    } 
 };
-void Dijkstra(vector<forward_list<egde>> graph,int begin,int end){
-    priority_queue<egde> pq;
-    map<int,int> from={{begin,0}};
-    for(auto e=graph[begin].begin();e!=graph[begin].end();e++){
-            pq.push(*e);
-    }
-    while(from.find(end)==from.end()&&!pq.empty()){
-        int vertex=pq.top().to;
-        if(from.find(vertex)==from.end()){
-            from[vertex]=pq.top().from;
-            for(auto e=graph[vertex].begin();e!=graph[vertex].end();e++){
-                pq.push(*e);
+unsigned long long dist[N];
+int vomit[N];
+vector<forward_list<egde>> graph(N);
+vector<set<int>> preorder(N);
+void Dijkstra(int begin){
+    queue<egde> q;//因为要找出所有最短路径，而不是找到一条就停止，所以就不必采用优先队列
+    dist[begin]=0;
+    int current=begin;
+    do{
+        for(auto e=graph[current].begin();e!=graph[current].end();e++){
+            if(dist[e->from]+e->cost<dist[e->to]){
+                dist[e->to]=dist[e->from]+e->cost;
+                preorder[e->to].clear();
+                preorder[e->to].insert(e->from);
+                q.push(*e);
+            }else if(dist[e->from]+e->cost==dist[e->to]){
+                preorder[e->to].insert(e->from);
+                q.push(*e);
             }
         }
-        pq.pop();
+        current=q.front().to;
+        q.pop();
+    }while(!q.empty());
+    for(auto a=preorder[4].begin();a!=preorder[4].end();a++){
+        cout<<*a<<" ";
     }
-    int v=from[end];
-    int min=1e9;
-    while(v!=1){
-        if(min>vomit[v])
-            min=vomit[v];
-        v=from[v];
-    }
-    cout<<min<<endl;
 }
 
 int main(){
     int n,m;
     int a,b,d;
     cin>>n>>m;
-    vector<forward_list<egde>> graph(n+1);
-    
     for(int i=0;i<m;i++){
         cin>>a>>b>>d;
         graph[a].push_front({a,b,d});
         graph[b].push_front({b,a,d});
     }
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++){
         cin>>vomit[i];
-    Dijkstra(graph,1,n);
+        dist[i]=UINT64_MAX;//顺便把dist初始化了
+    }
+    Dijkstra(1);
 }
