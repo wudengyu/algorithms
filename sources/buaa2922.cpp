2c5e601353e50d2de639c3c408d75e76731480b8
wudengyu <wudengyu@tom.com>
2021-02-04 00:47:46 +0800

    不用优先队列很慢

--- sources/buaa2922.cpp
+++ sources/buaa2922.cpp
@@ -13,31 +13,62 @@ struct egde{
     int cost;
 };
 unsigned long long dist[N];
-int vomit[N];
+struct comp{
+    bool operator()(int a,int b){
+        return dist[a]>dist[b];
+    }
+};
+int vomit[N],path[N];
 vector<forward_list<egde>> graph(N);
 vector<set<int>> preorder(N);
+int traversal(int begin,int end,int step,int vertex){
+    if(vertex==begin){
+        int min_vomit=1e9;
+        for(int i=1;i<=step-1;i++){
+            //cout<<path[i]<<" ";
+            if(vomit[path[i]]<min_vomit)
+                min_vomit=vomit[path[i]];
+        }
+        //cout<<"min_vomit="<<min_vomit<<endl;
+        for(int i=1;i<step-1;i++){
+            vomit[path[i]]-=min_vomit;
+        }
+        return min_vomit;
+    }else{
+        path[step]=vertex;
+        int returnvalue=0;
+        for(auto i=preorder[vertex].begin();i!=preorder[vertex].end();i++){
+            returnvalue+=traversal(begin,end,step+1,*i);
+        }
+        return returnvalue;
+    }
+}
 void Dijkstra(int begin){
-    queue<egde> q;//因为要找出所有最短路径，而不是找到一条就停止，所以就不必采用优先队列
+    priority_queue<int,vector<int>,comp> pq;
+    set<int> visited;
     dist[begin]=0;
-    int current=begin;
-    do{
+    pq.push(begin);
+    while(!pq.empty()){
+        int current;
+        do{
+            current=pq.top();
+            pq.pop();
+        }while(visited.find(current)!=visited.end()&&!pq.empty());
+        if(visited.find(current)!=visited.end()) break;
+        visited.insert(current);
         for(auto e=graph[current].begin();e!=graph[current].end();e++){
             if(dist[e->from]+e->cost<dist[e->to]){
                 dist[e->to]=dist[e->from]+e->cost;
                 preorder[e->to].clear();
                 preorder[e->to].insert(e->from);
-                q.push(*e);
+                pq.push(e->to);
             }else if(dist[e->from]+e->cost==dist[e->to]){
                 preorder[e->to].insert(e->from);
-                q.push(*e);
+                pq.push(e->to);
             }
         }
-        current=q.front().to;
-        q.pop();
-    }while(!q.empty());
-    for(auto a=preorder[4].begin();a!=preorder[4].end();a++){
-        cout<<*a<<" ";
     }
+    cout<<traversal(1,7,0,7)<<endl;
 }
 
 int main(){
