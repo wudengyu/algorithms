5524d894a501b68102764c465c4d13f279f33555
wudengyu <wudengyu@tom.com>
2021-02-08 17:37:02 +0800

    df

--- sources/buaa2922.cpp
+++ sources/buaa2922.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<cstring>
 #include<vector>
 #include<queue>
+#include<stack>
 #include<map>
 #include<set>
 #include<list>
@@ -12,8 +14,9 @@ struct egde{
     int from;
     int to;
     int cost;
-    int c=1e9;//容量
+    int capacity=1e9;//容量
     int f=0;//流
+    bool reverse=false;
     list<egde>::iterator rev;//反向边
 };
 struct vertex{
@@ -22,6 +25,7 @@ struct vertex{
     list<egde> adjoin;
 }graph[2*N+1];
 int vomit[N+1];
+int visited[2*N+1];
 long long dist[N+1];
 struct comp{
     bool operator()(list<egde>::iterator a,list<egde>::iterator b){
@@ -39,8 +43,7 @@ void Dijkstra(int begin){
         list<egde>::iterator current=pq.top();
         pq.pop();
         if(dist[current->from]+current->cost>dist[current->to]){
-            graph[current->from].adjoin.erase(current);
-            graph[current->to].adjoin.erase(current->rev);//反向边也删除
+            graph[current->to].adjoin.erase(current);
         }else{
             for(auto p=graph[current->to].adjoin.begin();p!=graph[current->to].adjoin.end();p++){
                 if(dist[p->from]+p->cost<dist[p->to]){
@@ -51,67 +54,66 @@ void Dijkstra(int begin){
         }
     }
 }
-void push(list<egde>::iterator side){
-    int delta=min(graph[side->from].e,side->c);
-    side->f+=delta;
-    graph[side->from].e-=delta;
-    graph[side->to].e+=delta;
-    
-}
-void generic_push_relabel(int s,int t,int length){
-    /*除了起点和终点，其余每个点都拆成两个点；*/
-    for(int i=2;i<length;i++){
-        graph[i].adjoin.push_front({i,N+i,0,vomit[i],0});
-        graph[N+i].adjoin.push_front({N+i,i,0,vomit[i],0});
-        graph[i].adjoin.front().rev=graph[N+i].adjoin.begin();
-        graph[N+i].adjoin.front().rev=graph[i].adjoin.begin();
-    }
-    graph[s].h=2*length-2;
-    for(auto p=graph[s].adjoin.begin();p!=graph[s].adjoin.end();p++){
-        p->f=p->c;
-        graph[p->to].e=p->c;
-        graph[s].e-=p->c;
-        graph[p->to].adjoin.push_front({p->to,p->from,p->cost,p->c,0,p});
-        p->rev=graph[p->to].adjoin.begin();
-    }
-    bool exists=true;
-    while(exists){
-        exists=false;
-        for(int i=1;i<=length;i++){
-            if(graph[i].e>0){
-                for(auto p=graph[i].adjoin.begin();p!=graph[i].adjoin.end();p++){
-                    if(p->c>0&&graph[i].h==graph[p->from].h+1)
-                }
+void splitvertex(int s,int t,int length){
+    for(int i=1;i<=length;i++){
+        if(i!=s&&i!=t){//除了起点和终点
+            graph[length+i-1].adjoin.swap(graph[i].adjoin);
+            for(auto p=graph[length+i-1].adjoin.begin();p!=graph[length+i-1].adjoin.end();p++){
+                p->from=length+i-1;
             }
+            graph[i].adjoin.push_front({from:i,to:length+i-1,cost:0,capacity:vomit[i]});
         }
+    }
 
+}
+int dfs(int s,int t,int length){
+    stack<list<egde>::iterator> st;
+    memset(visited,0,sizeof(int)*(2*N+1));
+    visited[s]=1;
+    for(auto p=graph[s].adjoin.begin();p!=graph[s].adjoin.end();p++){
+        if(p->capacity-p->f>0)
+            st.push(p);
+    }
+    list<egde>::iterator current;
+    bool exist=false;
+    while(!st.empty()){
+        current=st.top();
+        if(current->to==t){
+            exist=true;
+            break;
+        }
+        if(visited[current->to]==0){
+            for(auto p=graph[s].adjoin.begin();p!=graph[s].adjoin.end();p++){
+                if(p->capacity-p->f>0)
+                    st.push(p);
+            }
+            continue;
+        }else if({
+
+        }
     }
 }
-
 int main(){
     int n,m;
     int a,b,d;
     cin>>n>>m;
     for(int i=0;i<m;i++){
         cin>>a>>b>>d;
-        graph[a].adjoin.push_front({a,b,d});
-        graph[b].adjoin.push_front({b,a,d});
-        graph[a].adjoin.front().rev=graph[b].adjoin.begin();
-        graph[b].adjoin.front().rev=graph[a].adjoin.begin();
+        graph[a].adjoin.push_front({from:a,to:b,cost:d});
+        graph[b].adjoin.push_front({from:b,to:a,cost:d});
     }
     for(int i=1;i<=n;i++){
         cin>>vomit[i];
         dist[i]=__LONG_LONG_MAX__;//顺便把dist初始化了
     }
     Dijkstra(1);
-    generic_push_relabel(1,n,n);
+    splitvertex(1,n,n);
     for(int i=1;i<2*N+1;i++){
         if(!graph[i].adjoin.empty()){
             for(auto e=graph[i].adjoin.begin();e!=graph[i].adjoin.end();e++){
-                cout<<"("<<e->from<<","<<e->to<<")={"<<e->c<<","<<e->f<<"} ";
+                cout<<"("<<e->from<<","<<e->to<<")={"<<e->capacity<<","<<e->f<<"} ";
             }
             cout<<endl;
         }
     }
-    cout<<graph[1].e<<" "<<graph[1].h<<endl;
 }
