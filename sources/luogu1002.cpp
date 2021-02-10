6020deeae3764f394532a1c19ce94ffdc579e6de
wudengyu <wudengyu@tom.com>
2021-02-11 01:08:42 +0800

    s

---
+++ sources/luogu1002.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+bool horse(int hx,int hy,int x,int y){
+    if(x==0&&y==0)
+        return false;
+    return (abs(hx-x)==2&&abs(hy-y)==1)||(abs(hx-x)==1&&abs(hy-y)==2)||(hx==x&&hy==y);
+}
+int main(){
+    int bx,by,hx,hy;
+    int dp[21];
+    for(int i=0;i<=21;i++)
+        dp[i]=0;
+    cin>>bx>>by>>hx>>hy;
+    for(int i=bx;i>=0;i--){
+        if(i==bx)
+            dp[by]=1;
+        else
+            dp[by]=dp[by]==0?0:1;
+        for(int j=by;j>=0;j--){
+            if(horse(hx,hy,i,j))
+                dp[j]=0;
+            else
+                dp[j]+=dp[j+1];
+
+        }
+        for(int x=0;x<=by;x++)
+            cout<<dp[x]<<" ";
+        cout<<endl;
+    }
+    cout<<dp[0]<<endl;
+}
