bf39bbd69a6761ae73ace2e26f845f9b5efec995
wudengyu <wudengyu@tom.com>
2021-02-19 00:42:22 +0800

    1013

--- sources/luogu1013.cpp
+++ sources/luogu1013.cpp
@@ -1,36 +1,61 @@
 #include<iostream>
 using namespace std;
 string matrix[9][9];
-char row[9],col[9];
-int zero(int length){
-    int rindex=0,cindex=0;
+char alphabet[9]={'+','+','+','+','+','+','+','+','+'};
+int row[9]={0,0,0,0,0,0,0,0,0};//实际0、1、……等在原表中的行号；
+int col[9]={0,0,0,0,0,0,0,0,0};//实际0、1、……等在原表中的列号；
+void zero(int length){
+    int index;
     for(int i=1;i<length;i++){
-        rindex=i;
+        index=i;
         for(int j=1;j<length;j++){
             if(matrix[0][j]!=matrix[i][j]){
-                rindex=0;
+                index=0;
                 break;
             }
         }
-        if(rindex!=0)
+        if(index!=0)
             break;
     }
-    for(int i=1;i<length;i++){
-        cindex=i;
-        for(int j=1;j<length;j++){
-            if(matrix[j][0]!=matrix[j][i]){
-                cindex=0;
-                break;
-            }
+    alphabet[0]=matrix[index][0].at(0);
+    row[0]=index;
+    for(int i=0;i<length;i++){
+        if(alphabet[0]==matrix[0][i].at(0)){
+            col[0]=i;
         }
-        if(cindex!=0)
-            break;
     }
-    return matrix[0][rindex]==matrix[cindex][0]?rindex:0;
 }
-
+void one(int length){
+    for(int i=1;i<length;i++){
+        for(int j=1;j<length;j++){
+            if(matrix[i][j].length()==2&&matrix[i][j].at(1)==alphabet[0]){
+                alphabet[1]=matrix[i][j].at(0);
+                for(int k=0;k<length;k++){
+                    if(alphabet[1]==matrix[k][0].at(0)){
+                        row[1]=k;
+                    }
+                    if(alphabet[1]==matrix[0][k].at(0)){
+                        col[1]=k;
+                    }
+                }
+            }
+        }
+    }
+}
+void other(int length){
+    for(int x=2;x<length-1;x++){
+        alphabet[x]=matrix[row[1]][col[x-1]].at(0);
+        for(int k=0;k<length;k++){
+            if(alphabet[x]==matrix[k][0].at(0)){
+                row[x]=k;
+            }
+            if(alphabet[x]==matrix[0][k].at(0)){
+                col[x]=k;
+            }
+        }
+    }
+}
 int main(){
-
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
@@ -38,6 +63,16 @@ int main(){
             cin>>matrix[i][j];
         }
     }
-    cout<<zero(n)<<endl;
-
+    zero(n);
+    one(n);
+    other(n);
+    for(int i=0;i<n;i++)
+        cout<<alphabet[i];
+    cout<<endl;
+    for(int i=0;i<n;i++)
+        cout<<row[i];
+    cout<<endl;
+    for(int i=0;i<n;i++)
+        cout<<col[i];
+    cout<<endl;
 }
